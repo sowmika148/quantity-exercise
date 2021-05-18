943cdd047c5e9fb3915e21c3a0ff9fbe14606d5b
Tammana Sowmika <stammanathoughtworks.com@Tammanas-MacBook-Pro.local>
2021-05-18 12:17:26 +0530

    Sowmika | Req2 | Change Centimeter to Quantity

--- src/main/java/com/tw/pathashala/Quantity.java
+++ src/main/java/com/tw/pathashala/Quantity.java
@@ -1,18 +1,20 @@
 package com.tw.pathashala;
 
-public class Centimeter {
+public class Quantity {
 
     private final double magnitude;
+    private final Unit unit;
 
-    public Centimeter(double magnitude) {
+    public Quantity(double magnitude, Unit unit) {
         this.magnitude = magnitude;
+        this.unit = unit;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Centimeter that = (Centimeter) o;
+        Quantity that = (Quantity) o;
         return Double.compare(that.magnitude, magnitude) == 0;
     }
 }
---
+++ src/main/java/com/tw/pathashala/Unit.java
@@ -0,0 +1,5 @@
+package com.tw.pathashala;
+
+public enum Unit {
+    CM;
+}
